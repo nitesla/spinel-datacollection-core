15ed31af37b4f10a06f12fbe6ddaa0bc599d685d
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2022-02-12 07:31:55 +0100

    commit

---
+++ src/test/java/com/sabilogistics/api/sabilogisticapi/SabiLogisticApiApplicationTests.java
@@ -0,0 +1,14 @@
+//package com.sabilogistics.api.sabilogisticapi;
+//
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SabiLogisticApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
