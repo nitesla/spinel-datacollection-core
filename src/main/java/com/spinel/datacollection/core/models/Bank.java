2df680d833b3afcace6eb591993f2c1932b8a91f
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2022-02-13 09:24:13 +0100

    commit

---
+++ src/main/java/com/spinel/datacollection/core/models/Bank.java
@@ -0,0 +1,24 @@
+package com.spinel.datacollection.core.models;
+
+import com.sabi.framework.models.CoreEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+@Data
+@Entity
+public class Bank extends CoreEntity {
+
+
+    private String name;
+    private String code;
+
+    public Bank(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+}
---
+++ src/main/java/com/spinel/datacollection/core/models/Country.java
@@ -0,0 +1,26 @@
+package com.spinel.datacollection.core.models;
+
+
+import com.sabi.framework.models.CoreEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+@Data
+@Entity
+public class Country extends CoreEntity {
+
+    private String name;
+    private String code;
+
+
+    public Country(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+}
