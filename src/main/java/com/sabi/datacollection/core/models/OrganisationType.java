5405980ea396ee4aaca85ba2962f43e0126fc22b
toluadesola <t.toluadesola@gmail.com>
2022-02-25 13:48:41 +0100

    added OrganisationType.java

---
+++ src/main/java/com/sabi/datacollection/core/models/OrganisationType.java
@@ -0,0 +1,19 @@
+package com.sabi.datacollection.core.models;
+
+import com.sabi.framework.models.CoreEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+@Data
+@Entity
+public class OrganisationType extends CoreEntity {
+    private String name;
+    private String description;
+}
