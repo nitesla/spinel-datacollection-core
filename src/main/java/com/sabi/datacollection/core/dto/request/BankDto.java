2b1f5c41d4d93dd2335950ffc5cbc3827141271f
Ariyo-Agbaje Gbeminiyi <gbeminiyi.a@spinel.consulting>
2022-03-24 13:02:35 +0100

    Bank

---
+++ src/main/java/com/sabi/datacollection/core/dto/request/BankDto.java
@@ -0,0 +1,14 @@
+package com.sabi.datacollection.core.dto.request;
+
+
+import lombok.Data;
+
+@Data
+public class BankDto {
+
+    private Long id;
+
+    private String name;
+
+    private String code;
+}
---
+++ src/main/java/com/sabi/datacollection/core/dto/response/BankResponseDto.java
@@ -0,0 +1,34 @@
+package com.sabi.datacollection.core.dto.response;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BankResponseDto {
+
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    private Long createdBy;
+
+    private Long updatedBy;
+
+    private Boolean isActive;
+}
